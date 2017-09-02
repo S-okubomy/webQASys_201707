fb7b2ee0b858a9ef78b79ef11f14f4e195e5929d
okubo <okubo1504@gmail.com>
2017-09-02 23:54:55 +0900

    ・天気情報追加 ・JavaからPythonを実行できるように修正

--- src/main/java/com/app/controller/WebQaSysProtoController.java
+++ src/main/java/com/app/controller/WebQaSysProtoController.java
@@ -18,9 +18,13 @@ import org.springframework.stereotype.Controller;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
+
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -124,13 +128,22 @@ public class WebQaSysProtoController
     public String indexOld(Locale locale, IndexForm indexForm, Model model) {
         logger.info("Welcome home! The client locale is {}.", locale);
         
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-        String formattedDate = dateFormat.format(date);
+//        TimeZone tzn = TimeZone.getTimeZone("Asia/Tokyo");
+//        SimpleDateFormat fmt = new SimpleDateFormat();
+//        fmt.setTimeZone(tzn);
+//        Date datet = new Date();
+//        fmt.format(datet);
+        
+        // 日付型を日本時間にする。
+        ZonedDateTime nowZonedDt = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+        Date dateJp = Date.from(nowZonedDt.toInstant());
+        
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String fmtJpDate = fmt.format(dateJp);
+        model.addAttribute("serverTime", fmtJpDate);
         
         ResultQAModelDto resultQADto = new ResultQAModelDto();
         model.addAttribute("resultQADto", resultQADto);
-        model.addAttribute("serverTime", formattedDate );
         
         return "webQAindexOld";
     }
@@ -157,21 +170,25 @@ public class WebQaSysProtoController
     public String unchiku(Locale locale, IndexForm indexForm, Model model) throws Exception {
         logger.info("Welcome home! The client locale is {}.", locale);
         
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-        String formattedDate = dateFormat.format(date);
-        model.addAttribute("serverTime", formattedDate );
+        // 日付型を日本時間にする。
+        ZonedDateTime nowZonedDt = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+        Date dateJp = Date.from(nowZonedDt.toInstant());
+        
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String fmtJpDate = fmt.format(dateJp);
+        model.addAttribute("serverTime", fmtJpDate);
 
+        // 書式を変換
         SimpleDateFormat sdf1 = new SimpleDateFormat("M'月'd'日'");
-        String today = sdf1.format(date);
+        String today = sdf1.format(dateJp);
         
         String[] question = new String[1];
-        question[0] = today + "のうんちくは何ですか？";
+        question[0] = today + "のうんちくを教えてください。";
         
         List<AnsModelDto> resultAnsList = createInfoService.getCreateInfo(question, "unchiku.txt");
         model.addAttribute("resultAnsList", resultAnsList);
         
-        model.addAttribute("questionWeb", "今日（" + today + "）" + "のうんちく教えてわん");
+        model.addAttribute("questionWeb", "今日（" + today + "）" + "のうんちく教えてわん　※日付はサーバー設置場所");
 
         
         return "infopage1";
@@ -224,4 +241,137 @@ public class WebQaSysProtoController
 
         return "infopage1";
     }
+    
+    /**
+     * 今日のニュースを教えてください。
+     * @throws Exception 
+     */
+    @RequestMapping(value = "/news1", method = RequestMethod.GET)
+    public String news1(Locale locale, IndexForm indexForm, Model model) throws Exception {
+        logger.info("Welcome home! The client locale is {}.", locale);
+        
+        // 日付型を日本時間にする。
+        ZonedDateTime nowZonedDt = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+        Date dateJp = Date.from(nowZonedDt.toInstant());
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String fmtJpDate = fmt.format(dateJp);
+        model.addAttribute("serverTime", fmtJpDate);
+        
+        String[] question = new String[1];
+        question[0] = "今日のニュースを教えてください。";
+        
+        List<AnsModelDto> resultAnsList = createInfoService.getCreateInfo(question, "news1.txt");
+        model.addAttribute("resultAnsList", resultAnsList);
+        
+        model.addAttribute("questionWeb", question[0]);
+
+        return "infopage1";
+    }
+    
+    /**
+     * お勧めの映画を教えてください。
+     * @throws Exception 
+     */
+    @RequestMapping(value = "/movie1", method = RequestMethod.GET)
+    public String movie1(Locale locale, IndexForm indexForm, Model model) throws Exception {
+        logger.info("Welcome home! The client locale is {}.", locale);
+        
+        // 日付型を日本時間にする。
+        ZonedDateTime nowZonedDt = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+        Date dateJp = Date.from(nowZonedDt.toInstant());
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String fmtJpDate = fmt.format(dateJp);
+        model.addAttribute("serverTime", fmtJpDate);
+        
+        String[] question = new String[1];
+        question[0] = "お勧めの映画を教えてください。";
+        
+        List<AnsModelDto> resultAnsList = createInfoService.getCreateInfo(question, "movie1.txt");
+        model.addAttribute("resultAnsList", resultAnsList);
+        
+        model.addAttribute("questionWeb", question[0]);
+
+        return "infopage1";
+    }
+    
+    /**
+     * お勧めの映画を教えてください。
+     * @throws Exception 
+     */
+    @RequestMapping(value = "/tv1", method = RequestMethod.GET)
+    public String tv1(Locale locale, IndexForm indexForm, Model model) throws Exception {
+        logger.info("Welcome home! The client locale is {}.", locale);
+        
+        // 日付型を日本時間にする。
+        ZonedDateTime nowZonedDt = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+        Date dateJp = Date.from(nowZonedDt.toInstant());
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String fmtJpDate = fmt.format(dateJp);
+        model.addAttribute("serverTime", fmtJpDate);
+        
+        String[] question = new String[1];
+        question[0] = "お勧めのテレビ番組を教えてください。";
+        
+        List<AnsModelDto> resultAnsList = createInfoService.getCreateInfo(question, "tv1.txt");
+        model.addAttribute("resultAnsList", resultAnsList);
+        
+        model.addAttribute("questionWeb", question[0]);
+
+        return "infopage1";
+    }
+    
+    /**
+     * 明日の天気（Python 実行）。
+     * @throws Exception 
+     */
+    @RequestMapping(value = "/weather1", method = RequestMethod.GET)
+    public String Weather1(Locale locale, IndexForm indexForm, Model model) throws Exception {
+        logger.info("Welcome home! The client locale is {}.", locale);
+        
+        // 日付型を日本時間にする。
+        ZonedDateTime nowZonedDt = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+        Date dateJp = Date.from(nowZonedDt.toInstant());
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String fmtJpDate = fmt.format(dateJp);
+        model.addAttribute("serverTime", fmtJpDate);
+        
+        String[] question = new String[1];
+        question[0] = "今日の東京の天気を教えてください。";
+        
+        List<AnsModelDto> resultAnsList = createInfoService
+                .getPyCreateInfo(question, "weather1.txt", "weather1.py");
+        model.addAttribute("resultAnsList", resultAnsList);
+        
+        model.addAttribute("questionWeb", question[0]);
+
+        return "infopage1";
+    }
+    
+    /**
+     * メール 送信 明日の天気（Python 実行）。
+     * @throws Exception 
+     */
+    @RequestMapping(value = "/mailWeather1", method = RequestMethod.GET)
+    public String mailWeather1(Locale locale, IndexForm indexForm, Model model) throws Exception {
+        logger.info("Welcome home! The client locale is {}.", locale);
+        
+        // 日付型を日本時間にする。
+        ZonedDateTime nowZonedDt = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+        Date dateJp = Date.from(nowZonedDt.toInstant());
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+        String fmtJpDate = fmt.format(dateJp);
+        model.addAttribute("serverTime", fmtJpDate);
+        
+        String[] question = new String[1];
+        question[0] = "今日の東京の天気を教えてください。";
+        
+        List<AnsModelDto> resultAnsList = createInfoService
+                .getPyCreateInfo(question, "weather1.txt", "mailWeather.py");
+        model.addAttribute("resultAnsList", resultAnsList);
+        
+        model.addAttribute("questionWeb", question[0]);
+
+        return "infopage1";
+    }
+    
 }
